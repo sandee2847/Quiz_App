247c09e44a00b854cd060484c315ac8cba3190e7
sandee2847 <fugiganiyar14@gmail.com>
2023-04-22 15:44:04 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,77 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
 
 function App() {
+  const [questionIndex, setQuestionIndex] = useState(0);
+  const [score, setScore] = useState(0);
+  const [showResults, setShowResults] = useState(false);
+  const questions = [
+    {
+      questionText: "What is the capital of France?",
+      answerOptions: [
+        { answerText: "Berlin", isCorrect: false },
+        { answerText: "Paris", isCorrect: true },
+        { answerText: "London", isCorrect: false },
+        { answerText: "Madrid", isCorrect: false },
+      ],
+    },
+    {
+      questionText: "What is the largest country in the world?",
+      answerOptions: [
+        { answerText: "Russia", isCorrect: true },
+        { answerText: "Canada", isCorrect: false },
+        { answerText: "China", isCorrect: false },
+        { answerText: "United States", isCorrect: false },
+      ],
+    },
+    {
+      questionText: "What is the currency of Japan?",
+      answerOptions: [
+        { answerText: "Dollar", isCorrect: false },
+        { answerText: "Yen", isCorrect: true },
+        { answerText: "Euro", isCorrect: false },
+        { answerText: "Pound", isCorrect: false },
+      ],
+    },
+  ];
+
+  function handleAnswerButtonClick(isCorrect) {
+    if (isCorrect) {
+      setScore(score + 1);
+    }
+
+    if (questionIndex + 1 < questions.length) {
+      setQuestionIndex(questionIndex + 1);
+    } else {
+      setShowResults(true);
+    }
+  }
+
+  function handleRestartButtonClick() {
+    setQuestionIndex(0);
+    setScore(0);
+    setShowResults(false);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Quiz App</h1>
+      {showResults ? (
+        <div>
+          <h2>Results:</h2>
+          <p>You scored {score} out of {questions.length}.</p>
+          <button onClick={handleRestartButtonClick}>Restart Quiz</button>
+        </div>
+      ) : (
+        <div>
+          <h2>Question {questionIndex + 1}</h2>
+          <p>{questions[questionIndex].questionText}</p>
+          {questions[questionIndex].answerOptions.map((answerOption, index) => (
+            <button key={index} onClick={() => handleAnswerButtonClick(answerOption.isCorrect)}>
+              {answerOption.answerText}
+            </button>
+          ))}
+        </div>
+      )}
     </div>
   );
 }
